0c7f45d98a1917df1a49ecf10d61d87cf7553e0c
Piyush Shrivastava <piyush.shrivastava.9669@gmail.com>
2026-07-13 15:24:05 +0530

    Day 7 complete

---
+++ Day 6 Learning.py
@@ -0,0 +1,24 @@
+a = 2434353534
+naam = "piyush shrivastava"
+age = 18
+python_pasand_hai = True
+
+print("mera naam hai:",naam)
+print("meri age hai",age)
+print("kya mujhe python pasand hai?",python_pasand_hai)
+
+
+print("\n--- Computer se type check karte hain---")
+print(type(naam))
+print(type(age))
+print(type(python_pasand_hai))
+
+meri_fav_food = ["Mango", "Pizza", "Samosa"]
+
+print("\nmera favourite khana;",meri_fav_food)
+print("Food ka data type;",type(meri_fav_food))
+
+hafte_ke_din = (1, 2, 3, 4, 5, 6, 7)
+
+print("Hafte me itne din hote hai;",hafte_ke_din)
+print("Din ka data type;",type(hafte_ke_din))
