3c70460ee7cc7164f62d18f8085de6032f61ff0a
Piyush Shrivastava <piyush.shrivastava.9669@gmail.com>
2026-07-13 22:27:16 +0530

    added daily learning files

---
+++ Day 8 Learning.py
@@ -0,0 +1,13 @@
+a = 60
+b = 50
+print("The value of", a,"+",b, "is:",a+b )
+print("The value of", a,"//",b, "is:",a//b )
+print("The value of", a,"**",b, "is:",a**b )
+print("The value of", a,"/",b, "is:",a/b )
+print("The value of", a,"*",b, "is:",a*b )
+print("The value of", a,"-",b, "is:",a-b )
+print("The value of", a,"&",b, "is:",a%b )
+
+c = 150
+d = 350 
+print("The value of",c,"+",d, "is:",c+d)
