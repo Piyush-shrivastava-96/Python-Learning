58d4ef8064bf202a7a00dc02289435c1dbe381d6
Piyush Shrivastava <piyush.shrivastava.9669@gmail.com>
2026-07-14 16:49:20 +0530

    day 9 done

--- Day 9 Learning.py
+++ Day 9 Learning.py
@@ -1,7 +1,26 @@
-a = "12"
-b = "21"
+a = "1"
+b = "4" 
 print(int(a) + int(b))
 
-c = 1 
-d = 4 
-print(c+d)
+c = 5 
+d = 6
+
+print(c + d)
+
+s = "piyush"
+t = "shrivastava"
+
+print(s + t)
+
+x = 4.7 
+y = 1.2
+
+print(x - y)
+print(x + y)
+print(x * y)
+
+u = 3
+v = 7
+
+print(str(u) + str(v))
+print(int(u) + int(v))
