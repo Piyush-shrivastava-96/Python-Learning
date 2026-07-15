d2078b968dc9332212b0579c3d423993d9a3a3d5
Piyush Shrivastava <piyush.shrivastava.9669@gmail.com>
2026-07-15 08:06:52 +0530

    Merge branch 'main' of https://github.com/Piyush-shrivastava-96/Python-Learning

---
+++ Day 10 Learning.py
@@ -0,0 +1,23 @@
+s = input("Hey i am a piyush shrivastava and I want to ask you something you keep answering it:")
+n = input("where do you live bro:")
+a= input("Enter your name: ")
+print("its amazing your name is correct", a)
+
+c = input("enter your age;")
+d = input("enter your first fav number: ")
+v = input("enter your second fav numvber: ")
+print(int(d) + int(v))
+print(str(d) + str(v))
+print("thank you for your time")
+
+x = input("bhai rukna jra mujhe or bhi sawal puchne hai tumse:")
+m = input("apna college ka naam btate jao: ")
+y = input("thik bhai tum ab ja sakte ho:")
+
+q = int(input("enter your first mobile number:"))
+t = int(input("enter your second mobile number:"))
+
+print("oh bro your first and second mobile number addition is:", q + t)
+
+z = input("Have a nice day")
+print("thank you tata bye bye")
