bf7e639b66f675fdee0e41bc08c0954fe2575cb8
Piyush Shrivastava <piyush.shrivastava.9669@gmail.com>
2026-07-16 08:25:17 +0530

    Day 11 complete

---
+++ Day 11 Learning.py
@@ -0,0 +1,20 @@
+name = "piyush"
+
+print(name[4])
+print(name[2])
+print(name[0])
+
+
+Learn = """ Today I Learn Day 11  Python programming. I Learned about the following topics: Strings, data types, and operators. """
+print(Learn)
+
+for v in name:
+    print(v)
+
+    quotes = """ Success is not final,
+failure is not fatal:
+it is the courage to continue that counts."""
+
+print(quotes)
+for y in quotes:
+    print(y)
