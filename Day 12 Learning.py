224eb1856f31098aa6192ab84ba84e0e1ca0f34c
Piyush Shrivastava <piyush.shrivastava.9669@gmail.com>
2026-07-22 20:13:32 +0530

    Day 12 complete

--- Day 12 Learning.py
+++ Day 12 Learning.py
@@ -2,11 +2,30 @@ name = "piyush"
 print(len(name))
 he = len("piyush")
 print(he)
-B = "Hey i am learning python language"
-print(len(B))
 
-v = "Hello, world "
-print(len(v))
+the_word = "Rohan bhai"
+print("\n ---1. POSITIVE SLICING---")
+print(len(the_word))
+print(the_word[0:7])
+print(the_word[4:8])
+print(the_word[:9])
+print(the_word[3:])
 
-b = "Hello git hub and git hub is a best platform for learning and sharing code"
-print(len(b))
+print("\n ---2. NEGATIVE SLICING---")
+print(the_word[-5:-1])
+print(the_word[-10:-1])
+print(the_word[-7:-4])
+print(the_word[-8:-3])
+
+print("\n ---3. LEN() WITH NEGATIVE SLICING---")
+print(the_word[len(the_word)-5:len(the_word)-1])
+print(the_word[len(the_word)-9:len(the_word)-8])
+print(the_word[len(the_word)-10:len(the_word)-7])
+
+print("\n ---4. EMPATY OUTPUTS---")
+print(the_word[9:2])
+print(the_word[-7:-8])
+print(the_word[9:2])
+
+print(the_word[4:8:2])
+print(the_word[3:9:-2])
