2b3bbc1203bbc64872e751db19f0896471bf4788
Piyush Shrivastava <piyush.shrivastava.9669@gmail.com>
2026-07-29 15:52:00 +0530

    On Day 14, I Learned about If-Else Conditions,

--- Day 14 If-Else Learning.py
+++ Day 14 If-Else Learning.py
@@ -1,31 +1,60 @@
-print("1. If-Else use;")
+print("1. If-Else Program use;")
 
 student_marks = int(input("Aapke class 12th me kitne marks hai:"))
+if student_marks > 500:
+    print("Error")
+    exit()
+elif student_marks <= 0:
+    print("Error")
+    exit()
+
 sports_quota = input("kya apke pass sports quata hai (yes/no):")
 
 q = sports_quota
 print(sports_quota.lower())
+if sports_quota == "yes":
+     print("Status: I feel that you might get admitted now. ")
+     print("Because having a sports quota will admission quickly. Thankyou!")
+     exit()
+
 
 print("\nAdmission prosses: start now")
 
 if student_marks < 250:
     print("Admission prosses Denied!")
-    print("Because your marks are very low so i am really very sorry")
+    print("Because your marks are very low so i am really very sorry, We will not give you admission. Thankyou!")
 
-elif sports_quota == "yes":
-    print("Status: I feel that you might get admitted now. ")
-    print("Because having a sports quota will admission quickly.")
-    
+
+elif student_marks > 250 and student_marks <= 350:
+    print("Umm Okay! your marks are average.")
+    print("But sorry I can not give you admission because your marks are average. Thankyou!")
+
+elif student_marks >350 and student_marks <= 450:
+    print("Your marks are better than other students, But if the college list drops to this score, We will give you admission. Thankyou!")
+
+
 else:
     print("Status: Admission Granted!")
-    print("Checking your scholarship eligibilty and admissio selection prosses.")
-    int(input("Please,Write your 12th marks on computer screen ;"))
-    print("Okay you are selected because your marks are very high so you will come to collage tommarow.")
+    print("\nChecking your scholarship eligibilty and admissio selection prosses.")
+
+    re_enter_marks = int(input("\nPlease, Re-enter your 12th marks on computer screen, that you wrote above;"))
+
+    if re_enter_marks == student_marks:
+        print("\nOkay you are selected because your marks are very high so you will come to the collage tommarow.")
+
+    else : 
+        print("\nError! Please enter the same marks you entered previously. Thankyou!")
+
+        re_enter_again = int(input("\nPlease, Re-enter tha same marks on the computer screen;"))
+        if re_enter_again == student_marks:
+            print("\nOkay you are selected because your marks are very high so you will come to the collage tommarow.")
+        else:
+            print("\nYou have entered the wrong marks again, Admission Cancelled!")
 
 print("\n2. If-Else use;")
 
 num = 19 
-num = int(input("Enter your number:"))
+num = int(input("Enter your fav. number:"))
 if (num < 0):
     print("your number is negative")
 elif (num == 0):
